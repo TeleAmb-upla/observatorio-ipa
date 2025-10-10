4f1f0f03da974640f0da045ef681bd092f3c81cc
Erick Linares <erick.linares@gmail.com>
2025-10-10 14:08:23 -0300

    Add basic footer and privacy statement

--- src/observatorio_ipa/web/webapp/urls.py
+++ src/observatorio_ipa/web/webapp/urls.py
@@ -18,10 +18,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from debug_toolbar.toolbar import debug_toolbar_urls
+from .views import PrivacyView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
     # path("accounts/", include("allauth.urls")),
     path("", include("jobs.urls")),
+    path("privacy/", PrivacyView.as_view(), name="privacy"),
 ] + debug_toolbar_urls()
